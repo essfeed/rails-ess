4d7d8c8474ac02a413cbd9cbd5ac0a7e0ffc8a72
Marjan Povolni <marian.povolny@gmail.com>
2013-05-19 23:06:33 +0200

    Updated for more efficient pushing

--- lib/rails-ess/ess_feed_helper.rb
+++ lib/rails-ess/ess_feed_helper.rb
@@ -6,7 +6,10 @@ module ActionView
     module ESSFeedHelper
       def ess_feed(options = {}, &block)
         xml = eval("xml", block.binding)
-        ess = ESS::Maker.make(options, &block)
+        maker_options = options.clone
+        maker_options[:push => "false"]
+        maker_options[:aggregators => nil]
+        ess = ESS::Maker.make(maker_options, &block)
         output = ess.to_xml!
         if options[:aggregators] || options[:push]
           ESS::Pusher.push_to_aggregators(options.merge(:data => output))
