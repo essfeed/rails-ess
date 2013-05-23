7b9f643980068187876eab111be749e8deef0c8b
Marjan Povolni <marian.povolny@gmail.com>
2013-05-23 14:09:32 +0200

    Fixed an error: feeds were pushed twice instead of only once

--- lib/rails-ess/ess_feed_helper.rb
+++ lib/rails-ess/ess_feed_helper.rb
@@ -7,8 +7,8 @@ module ActionView
       def ess_feed(options = {}, &block)
         xml = eval("xml", block.binding)
         maker_options = options.clone
-        maker_options[:push => "false"]
-        maker_options[:aggregators => nil]
+        maker_options[:push] = false
+        maker_options[:aggregators] = nil
         ess = ESS::Maker.make(maker_options, &block)
         output = ess.to_xml!
         if options[:aggregators] || options[:push]
